2c3e3ad536ab3e5b186b57cfda98c7e4bb84888e
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-05-11 14:42:11 +0600

    feat: Update PopoverPanel styling for improved visual consistency

--- app/tools-compo/Header.js
+++ app/tools-compo/Header.js
@@ -97,7 +97,7 @@ export default function Header() {
 
                       <PopoverPanel
                         transition
-                        className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+                        className="absolute top-full -left-8 z-10 mt-4.5 w-screen max-w-md overflow-hidden rounded-xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
                       >
                         <div className="p-4">
                           {services.map((item) => (
